a14a0a0aeacec9cf30adde67e872561401f90bca
parzivval <123867960+parzivval@users.noreply.github.com>
2024-02-10 15:22:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let leftPos = 0;
-let bottomPos = 0;
+document.addEventListener('DOMContentLoaded', () => {
+    let leftPos = 0;
+    let bottomPos = 0;
 
-// When the "Yes" button is clicked, show an alert and then open Instagram.
-document.getElementById('yessir').addEventListener('click', () => {
-    alert('Что вершит судьбу человечества в этом мире? Некое незримое существо или закон, подобно длани господней, парящей над миром? По крайней мере, истинно то, что человек не властен даже над своей волей');
-    window.open('https://instagram.com/parziwal', '_blank');
+    document.getElementById('yessir').addEventListener('click', () => {
+        alert('Что вершит судьбу человечества в этом мире? Некое незримое существо или закон, подобно длани господней, парящей над миром? По крайней мере, истинно то, что человек не властен даже над своей волей');
+        window.open('https://instagram.com/parziwal', '_blank');
+    });
+
+    function moveButton() {
+        leftPos = Math.floor(Math.random() * (window.innerWidth - document.getElementById('nooo').offsetWidth));
+        bottomPos = Math.floor(Math.random() * (window.innerHeight - document.getElementById('nooo').offsetHeight));
+        document.getElementById('nooo').style.position = "absolute";
+        document.getElementById('nooo').style.left = leftPos + "px";
+        document.getElementById('nooo').style.top = bottomPos + "px";
+    }
+
+    document.getElementById('nooo').addEventListener('mouseover', moveButton);
+    document.getElementById('nooo').addEventListener('click', moveButton);
 });
-
-// Make the "No" button move on mouseover and click by changing its position randomly.
-function moveButton() {
-    leftPos = Math.floor(Math.random() * (window.innerWidth - document.getElementById('nooo').offsetWidth));
-    bottomPos = Math.floor(Math.random() * (window.innerHeight - document.getElementById('nooo').offsetHeight));
-    document.getElementById('nooo').style.position = "absolute";
-    document.getElementById('nooo').style.left = leftPos + "px";
-    document.getElementById('nooo').style.top = bottomPos + "px";
-}
-
-document.getElementById('nooo').addEventListener('mouseover', moveButton);
-document.getElementById('nooo').addEventListener('click', moveButton);
